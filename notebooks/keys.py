01ddbd58c262d199a146078a6c66cc663e77e2d5
ah239 <ah239@hdm-stuttgart.de>
2022-11-20 13:14:07 +0100

    Start des Programmierens + hinzufügen der Keys

---
+++ notebooks/keys.py
@@ -0,0 +1,19 @@
+# PYTHON FILE WITH TWITTER KEYS
+
+"""
+This file contains your Twitter keys
+
+    Replace the text with your keys
+    Keep the quotation marks
+
+"""
+
+consumer_key='lITt8gJZz6uW7eKsLNoWkfyTl'
+
+consumer_secret='Pypdc0jQvFAIZDThAdPNjCzNXllGFieLKlhCkbK6uQfbfxImpF'
+
+access_token='1331394891729997830-bOJCjO3yTYWXOaJIncBlYfeHu9Uhlj'
+
+access_token_secret='iJDcliQ7f8EgskHL1ZIc11j3czW51GrNeMKp6ld3UomRn'
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAF1giQEAAAAAxiY7xHvx2H4YpjAWpgPKefwSik4%3Dvw2z2mROIk10do29WjbCXf8DswTeiLzZRYgKuanqgCDGjnqncA'
